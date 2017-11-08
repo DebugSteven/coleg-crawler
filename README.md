60aa40e82b475d655b48b074a7a18faafe8425c9
Matt Parsons <parsonsmatt@gmail.com>
2017-11-07 22:09:56 -0700

    Rearrange the business

---
+++ README.md
@@ -0,0 +1,1 @@
+# coleg-crawler
---
+++ scraper/src/Scraper.hs
@@ -0,0 +1,9 @@
+module Scraper where
+
+import ClassyPrelude
+
+runScrape :: IO ()
+runScrape = do
+    putStrLn "Maybe take some command line arguments?"
+    putStrLn "Do a scrape job!"
+    putStrLn "Save the results."
