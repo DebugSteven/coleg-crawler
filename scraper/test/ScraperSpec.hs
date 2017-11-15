1c752f806c9bf6ce81afbfcd51523f0ff517a9ec
DebugSteven <debugsteven@gmail.com>
2017-11-15 16:03:27 -0700

    okay the tests actually work

--- scraper/test/ScraperSpec.hs
+++ scraper/test/ScraperSpec.hs
@@ -4,14 +4,15 @@ module ScraperSpec where
 
 import Test.Hspec
 import Scraper
-import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Char8 as L8
 import Control.Monad.IO.Class
 import Data.String.Conversions
 
 spec :: Spec
 spec = do
  describe "parsing correct links from base page" $ do 
+   website <- runIO $ L8.readFile "test/example/bill_page_1.html" 
    it "nextPage matches the link" $ do
-     nextPage `shouldBe` Just "/bill-search?field_sessions=10171&sort_bef_combine=field_bill_number%20ASC&page=1"
+     nextPage website `shouldBe` (Just "/bill-search?field_sessions=10171&sort_bef_combine=field_bill_number%20ASC&page=1")
    it "lastPage matches the link" $ do
-     lastPage `shouldBe` Just "/bill-search?field_sessions=10171&sort_bef_combine=field_bill_number%20ASC&page=31"
+     lastPage website `shouldBe` (Just "/bill-search?field_sessions=10171&sort_bef_combine=field_bill_number%20ASC&page=31")
