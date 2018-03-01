d26db1035784616b80f0ec4e4cf0be4d21bd2da2
DebugSteven <debugsteven@gmail.com>
2018-03-01 11:57:36 -0700

    tentatively the scraper is finished

--- scraper/src/NewScraper.hs
+++ scraper/src/NewScraper.hs
@@ -18,45 +18,55 @@ data Bill = Bill { billURL :: String
                  } deriving (Show, Eq)
 
 newtype IndexPageURL = IndexPageURL String deriving (Show, Eq)
-newtype BillPageURL = BillPageURL String deriving (Show, Eq)
+newtype BillPageURL = BillPageURL Text deriving (Show, Eq)
 
 newScrape = do
   -- we start with initial page with bill listings
   ws <- W.runSession (W.useBrowser (W.Chrome Nothing (Nothing) [] [] mempty) WC.defaultConfig) WS.getSession
   let run = W.runWD ws
   run $ do 
-    W.openPage "http://leg.colorado.gov/bill-search?field_sessions=10171&amp;sort_bef_combine=field_bill_number%20ASC&amp;page=31&page=0"
-    getNextLink "pager-next"
-    W.openPage "http://leg.colorado.gov/bills/hb17-1304"
+    scrapeLoop  
 
     where
+      firstIndex :: IndexPageURL
+      firstIndex = IndexPageURL "http://leg.colorado.gov/bill-search?field_sessions=10171&amp;sort_bef_combine=field_bill_number%20ASC&amp;page=31&page=0"
+
       scrapeLoop :: W.WD [Bill]
-      scrapeLoop = undefined
+      scrapeLoop = do
+        billList <- scrapeIndexPage firstIndex
+        traverse scrapeBillPage billList 
 
       scrapeIndexPage :: IndexPageURL -> W.WD [BillPageURL]
       scrapeIndexPage (IndexPageURL indexPage) = do
         W.openPage indexPage
-        if getNextLink "pager-next" == getNextLink "pager-last"
-        then undefined
-        else undefined 
+        currLink <- W.getCurrentURL
+        lastLink <- getNextLink "pager-last"
+        nextLink <- getNextLink "pager-next"
+        if currLink == lastLink
+        then getBillLinks
+        else do
+          links <- getBillLinks 
+          rest <- (scrapeIndexPage (IndexPageURL nextLink))
+          return (mappend links rest)
 
       scrapeBillPage :: BillPageURL -> W.WD Bill
       scrapeBillPage (BillPageURL billPage) = do
-        W.openPage billPage 
+        W.openPage $ unpack billPage 
         getBillInfo
 
+      getNextLink :: Text -> W.WD String
       getNextLink elemClass = do                    -- works for "pager-next" and "pager-last"
         getLi <- W.findElem $ W.ByClass elemClass   -- gets the li element that should contain an a tag child
         getA <- W.findElemFrom getLi $ W.ByTag "a"  -- gets the a child element from li
         link <- W.attr getA "href"                  -- gets the link!
-        return link
+        return $ unpack (fromMaybe "" link)
 
-      getBillLinks :: W.WD [Maybe Text] -- !! idk I don't want it to be a [Maybe Text] I want a [Text]
+      getBillLinks :: W.WD [BillPageURL] 
       getBillLinks = do
         getH4 <- W.findElems $ W.ByClass "node-title"                         -- gets list of bill node pointers
         getAs <- traverse (\elem -> W.findElemFrom elem $ W.ByTag "a") getH4  -- get a tags from nodes 
         links <- traverse (\elem -> W.attr elem "href") getAs                 -- get links for bills!
-        return links
+        return $ fmap BillPageURL (catMaybes links)
 
       getBillInfo :: W.WD Bill        -- this function is for real
       getBillInfo = do 
